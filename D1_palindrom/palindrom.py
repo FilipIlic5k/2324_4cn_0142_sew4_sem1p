d5c6c7d39acf1fa444904d95c59e0141c609b6b0
Filip Ilic <filipilic@Wireshark-Test-2.local>
2023-12-04 17:15:12 +0100

    to_base & get_dec_hex_palindrom implemented

--- D1_palindrom/palindrom.py
+++ D1_palindrom/palindrom.py
@@ -28,7 +28,8 @@ def is_palindrom(s: str):
     s = s.replace(" ", "").lower()
     return s == s[::-1]
 
-def is_palindrom_sentence(s:str):
+
+def is_palindrom_sentence(s: str):
     """
     Checks if a string is a palindrom.
     A palindrom is a string that is the same when read backwards.
@@ -51,6 +52,7 @@ def is_palindrom_sentence(s:str):
     s = s.replace(" ", "").replace(",", "").replace("!", "").replace("?", "").replace(".", "").lower()
     return is_palindrom(s)
 
+
 def palindrom_product(x):
     """
     Gets the biggest number (smaller that x), that is the product of 2 3-digit numbers.
@@ -68,13 +70,86 @@ def palindrom_product(x):
     99000099
 
     :param x: input number
-    :return: biggest palindrome number
+    :return: biggest palindrome product
     """
     n = 0
     for i in range(x, 0, -1):
         for j in range(i, 0, -1):
             x = i * j
             if x > n:
-                if is_palindrom(str(i * j)):
-                    n = i * j
+                if is_palindrom(str(x)):
+                    n = x
     return n
+
+
+import string
+
+digs = string.digits + string.ascii_uppercase
+
+
+def to_base(number: int, base: int) -> str:
+    """
+    Converts a number from decimal to a given base.
+
+    >>> num0 = to_base(1234,16); num0
+    '4D2'
+
+    >>> num1 = to_base(10, 2); num1
+    '1010'
+
+    >>> num2 = to_base(10, 3); num2
+    '101'
+
+    >>> num3 = to_base(10, 4); num3
+    '22'
+
+    >>> num4 = to_base(10, 5); num4
+    '20'
+
+    >>> num5= to_base(10, 11); num5
+    'A'
+
+    :param number: Number in 10-base system
+    :param base: Base to convert to
+    :return: Number converted to given base
+    """
+    if number < 0:
+        sign = -1
+    elif number == 0:
+        return digs[0]
+    else:
+        sign = 1
+
+    number *= sign
+    digits = []
+
+    while number:
+        digits.append(digs[number % base])
+        number = number // base
+
+    if sign < 0:
+        digits.append('-')
+
+    digits.reverse()
+    return ''.join(digits)
+
+
+def get_dec_hex_palindrom(x):
+    """
+    Gets the biggest number (smaller than x), that is a palindrom in the decimal system and the hexadecimal system.
+
+    >>> num1 = get_dec_hex_palindrom(1_000); num1
+    (979, '3D3')
+
+    >>> num2 = get_dec_hex_palindrom(5_000_000); num2
+    (2485842, '25EE52')
+
+    :param x: input number
+    :return: biggest palindrome number
+    """
+    n = 0
+    for i in range(x, 0, -1):
+        if is_palindrom(str(i)) and is_palindrom(to_base(i, 16)):
+            n = i
+            break
+    return n, to_base(n, 16)
