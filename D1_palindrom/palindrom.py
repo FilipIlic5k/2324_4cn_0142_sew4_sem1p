cf63c1ee4343376a450bc88118fc346b9006a252
Filip Ilic <filipilic@Wireshark-Test-2.local>
2023-12-04 15:45:53 +0100

    is_palindrom implemented

--- D1_palindrom/palindrom.py
+++ D1_palindrom/palindrom.py
@@ -7,7 +7,8 @@ __license__ = "GPL"
 __status__ = "Development"
 """
 
-def is_palindrom(s:str):
+
+def is_palindrom(s: str):
     """
     Checks if a string is a palindrom.
     A palindrom is a string that is the same when read backwards.
@@ -15,7 +16,7 @@ def is_palindrom(s:str):
     >>> str1 = is_palindrom("anna"); str1
     True
 
-    >>> str2 = is_palindrom(" anna"); str1
+    >>> str2 = is_palindrom(" anna"); str2
     True
 
     >>> str3 = is_palindrom("lotto"); str3
@@ -24,3 +25,5 @@ def is_palindrom(s:str):
     :param s: input string
     :return: boolean
     """
+    s = s.replace(" ", "").lower()
+    return s == s[::-1]
