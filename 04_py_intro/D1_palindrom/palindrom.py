88902013c0483cba81ccb7a18c98377efce298c7
Filip Ilic <filipilic@Wireshark-Test-2.local>
2024-01-07 18:20:31 +0100

    Added Pydoc Generic Info

--- 04_py_intro/D1_palindrom/palindrom.py
+++ 04_py_intro/D1_palindrom/palindrom.py
@@ -2,7 +2,7 @@
 __author__ = "Filip Ilic"
 __email__ = "filip.ilic@htl.rennweg.at"
 __version__ = "1.0.0"
-__copyright__ = "Copyright 2023"
+__copyright__ = "Copyright 2024"
 __license__ = "GPL"
 __status__ = "Development"
 """
