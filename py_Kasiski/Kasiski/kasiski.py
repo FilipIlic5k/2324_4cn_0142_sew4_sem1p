e64dd59d4bc9be8c5f1e91fc39587187a5f7aa9c
Filip Ilic <filipilic@Wireshark-Test-2.local>
2024-02-01 12:30:39 +0100

    allpos implemented

--- py_Kasiski/Kasiski/kasiski.py
+++ py_Kasiski/Kasiski/kasiski.py
@@ -6,6 +6,34 @@ __copyright__ = "Copyright 2024"
 __license__ = "GPL"
 __status__ = "Development"
 """
+from typing import List
+
+
 class Kasiski:
-    __init__(self, crypttext:str=""):
+    def __init__(self, crypttext:str=""):
+        """
+        Constructor. Initializes the crypttext.
+        :param crypttext:
+        """
         self.crypttext = crypttext
+
+
+    def allpos(self, text:str, teilstring:str) -> List[int]:
+        """
+        Berechnet die Positionen von teilstring in text.
+        Usage examples:
+        >>> k = Kasiski()
+        >>> k.allpos("heissajuchei, ein ei", "ei")
+        [1, 10, 14, 18]
+        >>> k.allpos("heissajuchei, ein ei", "hai")
+        []
+        """
+        pos = []
+        i = 0
+        while i < len(text):
+            if text[i:i+len(teilstring)] == teilstring:
+                pos.append(i)
+                i += len(teilstring)
+            else:
+                i += 1
+        return pos
