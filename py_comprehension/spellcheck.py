695bb18842b6f16edbd08bdaf5610bc4c4c54ba6
Filip Ilic <filipilic@MacBook-Pro.local>
2024-03-10 15:19:50 +0100

    edi1 implemented

--- py_comprehension/spellcheck.py
+++ py_comprehension/spellcheck.py
@@ -6,14 +6,16 @@ __copyright__ = "Copyright 2024"
 __license__ = "GPL"
 __status__ = "Development"
 """
+import string
 from collections.abc import Set
+from typing import List, Tuple
 
 
 def read_all_words(filename: str) -> Set[str]:
     """
     Reads all words from the given file and returns them as a set.
     :param filename:
-    :return:
+    :return: set of words
 
     >>> wc = len(read_all_words("words.txt")); wc
     3
@@ -36,4 +38,43 @@ def read_all_words(filename: str) -> Set[str]:
         return set()
 
 
+def split_word(word: str) -> List[Tuple[str, str]]:
+    """
+    Splits the given word into all possible combinations of two words.
+    :param word:
+    :return: list of tuples
 
+    >>> split_word("abc")
+    [('', 'abc'), ('a', 'bc'), ('ab', 'c'), ('abc', '')]
+    """
+    try:
+        return [(word[:i], word[i:]) for i in range(len(word) + 1)]
+    except Exception as e:
+        print(e)
+        return []
+
+
+def edit1(word: str) -> Set[str]:
+    """
+    Returns all words that are one edit away from the given word.
+    :param word:
+    :return:
+
+    >>> len(edit1('abc'))
+    182
+    >>> len(edit1(''))
+    26
+    """
+    try:
+        letters = string.ascii_lowercase
+        splits = split_word(word)
+        deletes = [a + b[1:] for a, b in splits if b]  # removes one letter
+        transposes = [a + b[1] + b[0] + b[2:] for a, b in splits if len(b) > 1]  # swaps two adjacent letters
+        replaces = [a + c + b[1:] for a, b in splits for c in letters if b]  # replaces every letter with every letter
+        # of the alphabet
+        inserts = [a + c + b for a, b in splits for c in letters]  # inserts every letter of the alphabet between two
+        # letters
+        return set(deletes + transposes + replaces + inserts)
+    except Exception as e:
+        print(e)
+        return set()
