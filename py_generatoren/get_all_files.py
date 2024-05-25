81c05794a3a2552d93a801cd0b1a910fe4155365
FilipIlic5k <ficko.ilic15@gmail.com>
2024-05-25 22:11:30 +0200

    get_all_files basic implemented

--- py_generatoren/get_all_files.py
+++ py_generatoren/get_all_files.py
@@ -8,16 +8,18 @@ __status__ = "Development"
 """
 
 import os
+from collections.abc import Iterable, Generator
+from typing import TextIO
 
 
-def get_all_files(path: str):
+def get_all_files(path: os.PathLike | str):
     """
     Generator function to yield all files in a given directory and its subdirectories.
     :param path: path to the directory
     :return: file in the directory
 
-    >>> p = get_all_files("C:/")
-    >>> next(p)
+    >>> list(get_all_files("../py_oop/"))
+    ['../py_oop/Fraction.py', '../py_oop/__pycache__/Fraction.cpython-312.pyc']
     """
     try:
         for entry in os.listdir(path):
@@ -37,7 +39,46 @@ def get_all_files(path: str):
         pass
 
 
+def open_files(filenames: Iterable[str]) -> Generator[TextIO, None, None]:
+    """
+    Generator function to open files and yield file handles.
+
+    :param filenames: Iterable of filenames
+    :return: Generator yielding file handles
+    """
+    for fn in filenames:
+        with open(fn) as f:
+            yield f
+
+
+def read_lines(files: Iterable[TextIO]) -> Generator[str, None, None]:
+    """
+    Generator function to read lines from file handles.
+
+    :param files: Iterable of file handles
+    :return: Generator yielding lines
+    """
+    for f in files:
+        for line in f:
+            yield line.rstrip()
+
+
+def print_lines(lines: Iterable[str]) -> None:
+    """
+    Function to print lines, stripping trailing whitespace.
+
+    :param lines: Iterable of lines
+    """
+    for line in lines:
+        print(line)
+
+
 if __name__ == "__main__":
-    directory_path = "/home/filip-ilic/"
+    directory_path = "/home/filip-ilic/Dokumente/Developer/Oracle/Oracle_XE"
     for file in get_all_files(directory_path):
         print(file)
+
+    fl = get_all_files(".")  # fl = "Liste" mit Filenamen
+    of = open_files(fl)  # of = "Liste" mit offenen Files
+    lines = read_lines(of)
+    print_lines(lines)
