6c1f9d58b73423582292449b2dbf5e8f9960718d
FilipIlic5k <ficko.ilic15@gmail.com>
2024-06-08 18:42:01 +0200

    reformat + fix first prime > 2⁵¹²

--- py_rsa/miller_rabin.py
+++ py_rsa/miller_rabin.py
@@ -9,15 +9,16 @@ __status__ = "Development"
 import random
 
 FIRST_100_PRIMES = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
-        31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
-        73, 79, 83, 89, 97, 101, 103, 107, 109, 113,
-        127, 131, 137, 139, 149, 151, 157, 163, 167, 173,
-        179, 181, 191, 193, 197, 199, 211, 223, 227, 229,
-        233, 239, 241, 251, 257, 263, 269, 271, 277, 281,
-        283, 293, 307, 311, 313, 317, 331, 337, 347, 349,
-        353, 359, 367, 373, 379, 383, 389, 397, 401, 409,
-        419, 421, 431, 433, 439, 443, 449, 457, 461, 463,
-        467, 479, 487, 491, 499, 503, 509, 521, 523, 541]
+                    31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
+                    73, 79, 83, 89, 97, 101, 103, 107, 109, 113,
+                    127, 131, 137, 139, 149, 151, 157, 163, 167, 173,
+                    179, 181, 191, 193, 197, 199, 211, 223, 227, 229,
+                    233, 239, 241, 251, 257, 263, 269, 271, 277, 281,
+                    283, 293, 307, 311, 313, 317, 331, 337, 347, 349,
+                    353, 359, 367, 373, 379, 383, 389, 397, 401, 409,
+                    419, 421, 431, 433, 439, 443, 449, 457, 461, 463,
+                    467, 479, 487, 491, 499, 503, 509, 521, 523, 541]
+
 
 def is_prime(number):
     """
@@ -25,22 +26,25 @@ def is_prime(number):
     """
     for prime in FIRST_100_PRIMES:
         if number % prime == 0:
-            return False
+            return number == prime
 
     return is_prim_millerrabin(number) == "probably prime"
+
 
 def is_prim_millerrabin(number, iterations=20):
     """
     Teste die ersten 100 Primzahlen auf Primzahl mittels Miller-Rabin-Test. Mindestens 20 Iterationen.
     """
+    if number % 2 == 0 or number < 2:
+        return "composite"
 
     # number - 1 = odd_part*2^exponent - 1
     exponent, odd_part = 0, number - 1
     while odd_part % 2 == 0:
-        exponent += 1
         odd_part //= 2
+        exponent += 1
 
-    def isComposite(base, odd_part, number, exponent):
+    def is_composite(base, odd_part, number, exponent):
         """
         Teste, ob die Zahl zusammengesetzt ist.
 
@@ -53,7 +57,7 @@ def is_prim_millerrabin(number, iterations=20):
         base_power = pow(base, odd_part, number)
         if base_power in (1, number - 1):
             return False
-        for _ in range(exponent-1):
+        for _ in range(exponent - 1):
             base_power = pow(base_power, 2, number)
             if base_power == number - 1:
                 return False
@@ -61,20 +65,24 @@ def is_prim_millerrabin(number, iterations=20):
 
     for _ in range(iterations):
         base = random.randint(2, number - 2)
-        if isComposite(base, odd_part, number, exponent):
+        if is_composite(base, odd_part, number, exponent):
             return "composite"
 
     return "probably prime"
 
-def generate_prime(bits):
+
+def generate_prime(bit_length):
     """
     Generiere eine Primzahl mit einer bestimmten Bitlänge.
     """
     while True:
-        prime_candidate = random.getrandbits(bits)
-        prime_candidate |=  (1 << (bits - 1)) | 1 # set the most significant bit and the least significant bit
+        prime_candidate = random.getrandbits(bit_length)
+        # Setze das höchste und niedrigste Bit, um sicherzustellen, dass die Zahl die richtige Bitlänge hat und
+        # ungerade ist
+        prime_candidate |= (1 << (bit_length - 1)) | 1
         if is_prime(prime_candidate):
             return prime_candidate
+
 
 if __name__ == '__main__':
     print("Teste Miller-Rabin-Algorithmus:")
@@ -82,17 +90,19 @@ if __name__ == '__main__':
     for number in test_numbers:
         print(f"Die Zahl {number} ist {'eine Primzahl' if is_prime(number) else 'keine Primzahl'}")
 
-    print("\nErste Primzahl mit 512 Bits:")
-    print(generate_prime(512))
-
+    print("\nErste Primzahl mit mehr als 512 Bits:")
+    number = pow(2, 512) + 1
+    while not is_prime(number):
+        number += 2
+    print(number)
 
     print("\nVersteckte Nachricht in 24566544301293569 als Binärzahl mot 12 Zeichen/Zeile:")
     binary = bin(24566544301293569)[2:]
     for i in range(0, len(binary), 12):
-        print(binary[i:i+12])
+        print(binary[i:i + 12])
 
     print("\nVersteckte Nachricht in 24566544301293569 als ASCII-Zeichen:")
-    ascii = "".join([chr(int(binary[i:i+8], 2)) for i in range(0, len(binary), 8)])
+    ascii = "".join([chr(int(binary[i:i + 8], 2)) for i in range(0, len(binary), 8)])
     print(ascii)
 
     print("\nNächst höhere Primzahl von 24566544301293569:")
@@ -115,7 +125,4 @@ if __name__ == '__main__':
     binary2 = bin(24566544301293587)[2:]
     xor = "".join([str(int(binary1[i]) ^ int(binary2[i])) for i in range(len(binary1))])
     for i in range(0, len(xor), 12):
-        print(xor[i:i+12])
-
-
-
+        print(xor[i:i + 12])
