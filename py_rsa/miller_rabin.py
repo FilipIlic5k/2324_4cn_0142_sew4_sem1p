0fc1a7f9c3b03f7742755217132d2b6d24bfd72b
FilipIlic5k <ficko.ilic15@gmail.com>
2024-06-08 17:23:44 +0200

    miller-rabin initialized

---
+++ py_rsa/miller_rabin.py
@@ -0,0 +1,8 @@
+"""
+__author__ = "Filip Ilic"
+__email__ = "filip.ilic@htl.rennweg.at"
+__version__ = "1.0.0"
+__copyright__ = "Copyright 2024"
+__license__ = "GPL"
+__status__ = "Development"
+"""
